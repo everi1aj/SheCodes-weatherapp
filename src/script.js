a776840f19064156c7daa52b80f34d257da8a819
everi1aj <71519711+everi1aj@users.noreply.github.com>
2020-10-19 15:43:28 -0400

    added hourly weather forecast

--- src/script.js
+++ src/script.js
@@ -22,6 +22,20 @@ function formatDate(date) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function formatHours(timestamp) {
+  let date = new Date(timestamp);
+  let hours = ((date.getHours() + 11) % 12) + 1;
+  if (hours < 12) {
+    hours = `${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+
+    return `${hours}:${minutes}`;
+  }
+}
+
 function showTemperature(response) {
   document.querySelector("#city").innerHTML = response.data.name;
   let temperatureElement = document.querySelector("#temperature");
@@ -77,10 +91,41 @@ fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
+function displayForecast(response) {
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = null;
+  let forecast = null;
+
+  for (let index = 0; index < 6; index++) {
+    forecast = response.data.list[index];
+    forecastElement.innerHTML += `
+              <div class="col-2">
+              <h3>
+              ${formatHours(forecast.dt * 1000)}
+              </h3>
+              <img
+              src="http://openweathermap.org/img/wn/${
+                forecast.weather[0].icon
+              }@2x.png"
+               />
+              <div class="weather-forecast-temperature">
+                <strong>
+                ${Math.round(forecast.main.temp_max)}°
+                </strong>/
+                ${Math.round(forecast.main.temp_min)}°
+              </div>
+            </div>
+            `;
+  }
+}
+
 function searchCity(city) {
   let apiKey = "42e6bc06b4b0dadbd7f47c9b7345f143";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(showTemperature);
+
+  apiURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiURL).then(displayForecast);
 }
 
 function handleSubmit(event) {
